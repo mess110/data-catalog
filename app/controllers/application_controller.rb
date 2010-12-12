3026939f1be1b656f2e986bc668175dec3c8f949
David James <djames@sunlightfoundation.com>
2010-12-11 19:26:06 -0500

    Setup Google analytics

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,10 +1,16 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
 
+  before_filter :set_analytics
+
   protected
 
   def render_404
     render 'public/404.html', :status => 404, :layout => false
+  end
+
+  def set_analytics
+    @analytics_key = DataCatalog::Application.config.analytics_key
   end
 
 end
---
+++ config/initializers/analytics.rb
@@ -0,0 +1,2 @@
+DataCatalog::Application.config.analytics_key = YAML::load_file(
+  Rails.root.join('config', 'analytics.yml'))['key']
