9aa2ba69d17a73faf6a47ff30c25619e0e02ffe9
David James <djames@sunlightfoundation.com>
2011-01-24 00:24:57 -0500

    minor: satiate TextMate's warnings re: ==

--- spec/models/catalog_spec.rb
+++ spec/models/catalog_spec.rb
@@ -10,22 +10,22 @@ describe Catalog do
     end
 
     it "no owners or curators" do
-      @catalog.curators.to_a.should == []
-      @catalog.owners.to_a.should == []
+      @catalog.curators.to_a.should == [] &&
+        @catalog.owners.to_a.should == []
     end
 
     it "with curator" do
       @user = Factory.create(:user)
       @user.curated_catalogs = [@catalog]
-      @catalog.curators.to_a.should == [@user]
-      @catalog.owners.to_a.should == []
+      @catalog.curators.to_a.should == [@user] &&
+        @catalog.owners.to_a.should == []
     end
 
     it "with owner" do
       @user = Factory.create(:user)
       @user.owned_catalogs = [@catalog]
-      @catalog.curators.to_a.should == []
-      @catalog.owners.to_a.should == [@user]
+      @catalog.curators.to_a.should == [] &&
+        @catalog.owners.to_a.should == [@user]
     end
   end
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -21,22 +21,22 @@ describe User do
     end
 
     it "no owned or curated catalogs" do
-      @user.curated_catalogs.to_a.should == []
-      @user.owned_catalogs.to_a.should == []
+      @user.curated_catalogs.to_a.should == [] &&
+        @user.owned_catalogs.to_a.should == []
     end
 
     it "with curated catalog" do
       @catalog = Factory.create(:catalog)
       @catalog.curators = [@user]
-      @user.curated_catalogs.to_a.should == [@catalog]
-      @user.owned_catalogs.to_a.should == []
+      @user.curated_catalogs.to_a.should == [@catalog] &&
+        @user.owned_catalogs.to_a.should == []
     end
 
     it "with owned catalog" do
       @catalog = Factory.create(:catalog)
       @catalog.owners = [@user]
-      @user.curated_catalogs.to_a.should == []
-      @user.owned_catalogs.to_a.should == [@catalog]
+      @user.curated_catalogs.to_a.should == [] &&
+        @user.owned_catalogs.to_a.should == [@catalog]
     end
   end
 end
