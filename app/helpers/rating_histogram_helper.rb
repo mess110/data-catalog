9ad15b694a0503c5fe6c10058e860e2dce57cf05
David James <djames@sunlightfoundation.com>
2011-02-02 09:22:30 -0500

    minor: refactor rating histogram

--- app/helpers/rating_histogram_helper.rb
+++ app/helpers/rating_histogram_helper.rb
@@ -1,10 +1,12 @@
-# encoding: UTF-8
 module RatingHistogramHelper
 
   BASE_URL = 'http://chart.apis.google.com/chart'
   DIMENSIONS = '165x110'
   SUBFOLDER = 'data_set_rating_histograms'
   COLOR = '0B6E8E'
+  X_LABELS = "|1*|2*|3*|4*|5*" # "|poor|fair|average|good|excellent"
+  FONT_SIZE = 12
+  LABEL_COLOR = '202020'
 
   def rating_histogram_image_tag(data_set_id, field_name, image_options = {})
     filename = rating_histogram_cached_filename(data_set_id, field_name)
@@ -33,25 +35,21 @@ module RatingHistogramHelper
   #    &chd=t:4,12,15,9,3
   def rating_histogram_url(data_set_id, field_name)
     data_set = DataSet.criteria.id(data_set_id).first
-    unless data_set
-      raise "Could not find DataSet with id: #{data_set_id}"
-    end
+    raise "Could not find DataSet with id: #{data_set_id}" unless data_set
     values = data_set[field_name]['bins']
-    values_max = round_up(values.max, 2)
-    x_labels = "|1*|2*|3*|4*|5*" # "|poor|fair|average|good|excellent"
-    y_labels = values_max > 0 ? "|0|#{values_max / 2}|#{values_max}" : "|0"
-    label_color = "202020"
-    font_size = "12"
+    max_value = round_up(values.max, 2)
+    y_labels = max_value > 0 ? "|0|#{max_value / 2}|#{max_value}" : "|0"
+    label_color = ""
     BASE_URL +
-      "?chxl=0:#{x_labels}|1:#{y_labels}" +
-      "&chxr=0,1,5|1,#{values_max}" +
-      "&chxs=0,#{label_color},#{font_size},0,l|1,#{label_color},#{font_size},0,l" +
+      "?chxl=0:#{X_LABELS}|1:#{y_labels}" +
+      "&chxr=0,1,5|1,#{max_value}" +
+      "&chxs=0,#{LABEL_COLOR},#{FONT_SIZE},0,l|1,#{LABEL_COLOR},#{FONT_SIZE},0,l" +
       "&chxt=x,y" +
       "&chbh=a" +
       "&chs=#{DIMENSIONS}" +
       "&cht=bvs" +
       "&chco=#{COLOR}" +
-      "&chds=0,#{values_max}" +
+      "&chds=0,#{max_value}" +
       "&chd=t:#{values.join(',')}"
   end
 
